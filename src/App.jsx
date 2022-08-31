73e17b88a2724e376dfb3b62589b7753284089fe
Zoe Lanz <zoe_lanz@hotmail.com>
2022-08-30 23:57:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar/NavBar'
+import './App.scss'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import './NavBar.scss'
+
+
+const NavBar = () => {
+  return (
+    <nav className="flex place-content-between items-center	 px-16 py-6 bg-white">
+        <div className='name'>
+            LUCRECIA MAYA
+        </div>
+        <div className=' gap-8 '>
+            <ul className="flex gap-8 pages tracking-wide">
+                <li><a href="">Home</a></li>
+                <li><a href="">Work</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
